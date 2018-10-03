ec6d666a4e8b307f0a30cb1c39dc61722e629c2b
barry.c.schechter <barry.c.schechter@gmail.com>
2018-10-03 20:10:06 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -5,9 +5,3 @@ function whileLoop(n) {
    }
  return "done"
 }
-function forLoop(array) {
-  for (i = 0; i < 25; i++) {
-  array[i] = 'I am ''$(i)'' strange loop${i === 1 ? '' : 's''}
-  }
-  return array
-}
