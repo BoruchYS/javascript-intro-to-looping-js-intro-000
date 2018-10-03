662e900874bedfeac0b1c20aab79a9c6ff382eca
barry.c.schechter <barry.c.schechter@gmail.com>
2018-10-03 20:38:04 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -7,7 +7,7 @@ function whileLoop(n) {
 }
 function forLoop(array){
   for (i = 0; i < 25; i++) {
-  var  s = i === 1 ? "I am 1 strange loop." : `I am ${i} strange loops.`;
+    s = i === 1 ? "I am 1 strange loop." : `I am ${i} strange loops.`;
     array[i] = s
   }
   return array
